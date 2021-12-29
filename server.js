cdb342b34fa5ad7f7cd3871218d704a210b842fe
alotfey <me@alotfey.com>
2021-12-29 12:32:29 -0500

    add route and express code

--- server.js
+++ server.js
@@ -1,8 +1,60 @@
+// import modules
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const notes = require('./db/db.json')
+
+// express app
+
 const app = express();
 
+// express public directory
+public = app.use(express.static('public'))
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
-// setup public directory
+// GET /notes 
+app.get('/notes', (req,res) =>{
+    res.sendFile('notes.html', { root: './public' });
+})
 
-app.use(express.static(__dirname + 'public'));
+// GET *
+app.get('/', (req,res) =>{
+    res.sendFile('index.html', { root: './public' });
+})
 
+// GET /api/notes read all file in db
+
+app.get('/api/notes' ,(req,res) => {
+    res.json(notes)
+})
+
+
+// POST /api/notes for receive a new note to save on the request body, add it to the db.json file
+function createNewNote(body,notesArray) {
+    const note = body;
+    notesArray.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+
+}
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes)
+    res.json(note)
+})
+  
+
+
+// server port 3001
+
+app.listen(3001, () => {
+    console.log('Web server start on port 3001');
+})
